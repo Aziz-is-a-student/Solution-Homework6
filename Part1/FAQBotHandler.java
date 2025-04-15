75bc3b6dcba22678877ddecc8ed8810ae1448fe7
Aziz <azizrasiodv99@gmail.com>
2025-04-15 17:16:08 +0500

    Third commit

--- Part1/FAQBotHandler.java
+++ Part1/FAQBotHandler.java
@@ -1,4 +1,12 @@
 package Part1;
 
-public class FAQBotHandler {
+public class FAQBotHandler extends SupportHandler{
+
+
+    @Override
+    public void handle(String question) {
+        if (question.equals("password reset")){
+            System.out.println("FAQBot сan't process the " +question + " — submit for manual processing.");
+        }
+    }
 }
