3e826588f1447879d231609f91b436dc82917c78
Aziz <azizrasiodv99@gmail.com>
2025-04-15 17:30:42 +0500

    Fourth commit

--- Part1/FAQBotHandler.java
+++ Part1/FAQBotHandler.java
@@ -4,9 +4,14 @@ public class FAQBotHandler extends SupportHandler{
 
 
     @Override
-    public void handle(String question) {
-        if (question.equals("password reset")){
-            System.out.println("FAQBot сan't process the " +question + " — submit for manual processing.");
+    public void handle(String problem) {
+        if ("password reset".equalsIgnoreCase(problem) || "reset password".equalsIgnoreCase(problem ){
+            System.out.println("FAQBot сan't process the " +problem + " — submit for manual processing.");
+        } else if (nextHandler != null) {
+            nextHandler.handle(problem);
+        } else {
+            System.out.println("[FAQBot] Cannot handle " + problem + " — escalate manually");
         }
+
     }
 }
