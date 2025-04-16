aeab5b9d940170840f7a59bb604269f72ee2e241
Aziz <azizrasiodv99@gmail.com>
2025-04-16 21:17:23 +0500

    15 commit

--- Part2/Client.java
+++ Part2/Client.java
@@ -1,4 +1,22 @@
 package Part2;
 
 public class Client {
+    public static void main(String[] args) {
+        Light light = new Light();
+        Thermostat thermostat = new Thermostat();
+
+        Command turnOnLightCommand = new TurnOnLightCommand(light);
+        Command setThermostatCommand = new SetThermostatCommand(thermostat, 22);
+
+        SmartHomeRemoteControl remote = new SmartHomeRemoteControl();
+
+        remote.addCommand("light", turnOnLightCommand);
+        remote.addCommand("thermostat", setThermostatCommand);
+
+        remote.pressButton("light");
+        remote.pressButton("thermostat");
+
+        System.out.println("Undo last command");
+        remote.undoButton("thermostat");
+    }
 }
